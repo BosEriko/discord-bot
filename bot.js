135257ef095d8b0f34452d30d8ef1408fdb88c55
BosEriko <bos.eriko@gmail.com>
2018-11-21 20:05:41 +0800

    test number 5

--- bot.js
+++ bot.js
@@ -9,15 +9,9 @@ client.on('message', message => {
     if (message.content === 'ping') {
         message.reply('pong');
     }
-});
-
-client.on('message', message => {
     if (message.content === 'pung') {
         message.reply('peng');
     }
-});
-
-client.on('message', message => {
     if(message.content === 'seishun') {
         message.reply('https://www.youtube.com/watch?v=3g9ihd-d2UY');
     }
