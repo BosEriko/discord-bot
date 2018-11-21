0f87321a1cd4e8277f65bd5a6278f3171ef7e494
BosEriko <bos.eriko@gmail.com>
2018-11-21 20:52:45 +0800

    Added rich embed test

--- bot.js
+++ bot.js
@@ -6,14 +6,26 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+    // Help Message
     if (message.content === 'ka!help') {
         message.reply('No one will help you.');
     }
+    // Show Avatar
     if (message.content === 'ka!avatar') {
         message.reply(message.author.avatarURL);
     }
+    // WOW
     if (message.content === 'ka!wow') {
         message.channel.send('WOW');
+    }
+    // Rich Embed
+    if (message.content === 'ka!wowzer') {
+        const embed = new RichEmbed()
+            .setTitle('WOWZER')
+            .setColor(0xFF0000)
+            .setDescription('RICH EMBED! :meguface:');
+        // Send the embed to the same channel as the message
+        message.channel.send(embed);
     }
 });
 
