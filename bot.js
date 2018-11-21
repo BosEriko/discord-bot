95e6260b835ff909f4ebd405dd027027cb73926b
BosEriko <bos.eriko@gmail.com>
2018-11-21 19:58:01 +0800

    Test number 3

--- bot.js
+++ bot.js
@@ -2,13 +2,17 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Bot is ready.');
 });
 
 client.on('message', message => {
     if (message.content === 'ping') {
         message.reply('pong');
-    }else if(message.content === 'seishun') {
+    }
+});
+
+client.on('message', message => {
+    if(message.content === 'seishun') {
         message.reply('https://www.youtube.com/watch?v=3g9ihd-d2UY');
     }
 });
