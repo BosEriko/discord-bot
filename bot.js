a88674b952cd6bc68c684ddd662bc572277fefb8
BosEriko <bos.eriko@gmail.com>
2018-11-25 12:19:32 +0800

    Checking out the contents of guildmemberadd

--- bot.js
+++ bot.js
@@ -27,12 +27,7 @@ Client.on('ready', () => {
 
 // New User
 Client.on('guildMemberAdd', member => {
-    // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.find(ch => ch.name === 'kuru-anime');
-    // Do nothing if the channel wasn't found on this server
-    if (!channel) return;
-    // Send the message, mentioning the member
-    channel.send(`Welcome to the server, ${member}`);
+    console.log("member =============>", member);
 });
 
 
