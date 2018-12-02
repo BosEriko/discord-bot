75dfc4ca0da034a03a4102ff489d8457da2be5a9
BosEriko <bos.eriko@gmail.com>
2018-12-02 13:42:00 +0800

    Spinning up the hamster...

--- bot.js
+++ bot.js
@@ -39,6 +39,30 @@ const funHelp = `
 **${symbolCommand}rules** Show the server rules
 `;
 
+// 8Ball answers
+const eightBall = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",
+];
+
 // Fortune Cookie Answers
 // http://www.fortunecookiemessage.com/archive.php
 const fortuneCookie = [
@@ -240,7 +264,7 @@ Client.on('message', message => {
                 break;
             // 8-ball
             case symbolCommand + '8ball':
-                message.reply(parameter);
+                message.reply('My answer to "' + parameter + '" is "' + eightBall[Math.floor(Math.random() * eightBall.length)] + '"');
                 break;
             // Show Help
             case symbolCommand + 'help':
