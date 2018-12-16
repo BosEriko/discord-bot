40ff238ceba1e6e15a386f832f005a40de4f9c7c
BosEriko <bos.eriko@gmail.com>
2018-12-16 18:42:46 +0800

    WTF is this.

--- bot.js
+++ bot.js
@@ -278,6 +278,7 @@ Client.on('message', message => {
     // Kuru Election
     if (message.channel.name === 'kuru-election' && Client.user.id !== message.author.id) {
         message.reply(electionDataValue);
+        console.log(message);
     }
     // Rabbit Post
     if (message.channel.name === 'rabbit-post' && Client.user.id !== message.author.id) {
