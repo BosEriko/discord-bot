ffdc1bf74ec60150dd55f49df2e9fbc7bb938d59
BosEriko <bos.eriko@gmail.com>
2018-12-17 16:02:57 +0800

    herpderp

--- bot-post.js
+++ bot-post.js
@@ -3,8 +3,8 @@ exports.botPost = (message, symbolCommand) => {
     const parameter = message.content.replace(command + " ", "");
     if (message.author.id === '230249439481167872') {
         if(message.content.startsWith(symbolCommand + 'bot-post')) {
-            message.channel.send(parameter);
             message.delete();
+            message.channel.send(parameter);
         }
     }
 };
