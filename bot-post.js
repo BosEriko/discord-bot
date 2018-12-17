810dcad1be0a360e872b959de11eb7a6c0cef91f
BosEriko <bos.eriko@gmail.com>
2018-12-17 16:05:45 +0800

    Stuff

--- bot-post.js
+++ bot-post.js
@@ -2,7 +2,7 @@ exports.botPost = (message, symbolCommand) => {
     const command   = message.content.split(" ")[0];
     const parameter = message.content.replace(command + " ", "");
     if (message.author.id === '230249439481167872') {
-        if(message.content.startsWith(symbolCommand + 'bot-post')) {
+        if(message.content.startsWith(symbolCommand + 'bp')) {
             message.delete();
             message.channel.send(parameter);
         }
