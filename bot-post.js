6518c2d3c1c9f1be5c9a619f69d08ff91b4f8ee8
BosEriko <bos.eriko@gmail.com>
2018-12-17 15:56:56 +0800

    This is not the commit message you are looking for

--- bot-post.js
+++ bot-post.js
@@ -1,6 +1,6 @@
 exports.botPost = (message, symbolCommand) => {
-    const command   = message.cleanContent.split(" ")[0];
-    const parameter = message.cleanContent.replace(command + " ", "");
+    const command   = message.content.split(" ")[0];
+    const parameter = message.content.replace(command + " ", "");
     if (message.author.id === '230249439481167872') {
         if(message.content.startsWith(symbolCommand + 'bot-post')) {
             message.channel.send(parameter);
