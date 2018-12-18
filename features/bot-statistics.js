f30fdd85b60e9f27e2eaec2a3dc12a47945c7a81
BosEriko <bos.eriko@gmail.com>
2018-12-18 18:10:29 +0800

    QuickFix.

---
+++ features/bot-statistics.js
@@ -0,0 +1,26 @@
+exports.botStatistics = (message, Client, firebase, symbolCommand) => {
+    // Firebase Data
+    const database = firebase.database();
+    const messageCount = database.ref().child('statistics/' + message.author.id + '/message_count');
+    let messageCountValue;
+
+    // Logic
+    messageCount.on("value", snap => {
+        if(!snap.exists()){
+            database.ref('statistics/' + message.author.id).set({
+                message_count: 0
+            })
+        };
+        messageCountValue = snap.val();
+    });
+    database.ref('statistics/' + message.author.id).set({
+        message_count: messageCountValue++
+    });
+
+    // The Main Stuff
+    if (Client.user.id !== message.author.id) {
+        if (message.content.startsWith(symbolCommand + 'message-count')) {
+            message.reply(messageCountValue);
+        }
+    }
+};
