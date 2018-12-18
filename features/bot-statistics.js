0396e8c78c2bc46c34b8368f846ccc5ad1b1cf04
BosEriko <bos.eriko@gmail.com>
2018-12-18 18:22:02 +0800

    herpderp

--- features/bot-statistics.js
+++ features/bot-statistics.js
@@ -16,6 +16,7 @@ exports.botStatistics = (message, Client, firebase, symbolCommand) => {
     // database.ref('statistics/' + message.author.id).set({
     //     message_count: messageCountValue++
     // });
+    
 
     // The Main Stuff
     if (Client.user.id !== message.author.id) {
