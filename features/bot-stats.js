27098cf96c144ae85a41a71c42e950eb9ef70fe9
BosEriko <bos.eriko@gmail.com>
2018-12-19 15:12:16 +0800

    Fixed mispeling

--- features/bot-stats.js
+++ features/bot-stats.js
@@ -3,16 +3,21 @@ exports.botStats = (message, Client, firebase, symbolCommand) => {
     const database = firebase.database();
     const statsData = database.ref().child('stats/' + message.author.id + '/data/message_count');
     statsData.once('value').then(snap => {
-        var statsDataMessage = snap.exists() ? snap.val() : 0;
+        let statsDataMessage = snap.exists() ? snap.val() : 0;
         database.ref('stats/' + message.author.id + '/data').set({
             message_count: statsDataMessage + 1
         });
     });
+    const statsDataMessageShow = () => {
+        statsData.once('value').then(snap => {
+            return snap.exists() ? snap.val() : 0;
+        }
+    }
 
     // The Main Stuff
     if (Client.user.id !== message.author.id) {
         if (message.content.startsWith(symbolCommand + 'st-message')) {
-            message.reply(statsDataMessage);
+            message.reply(statsDataMessageShow());
         }
     }
 };
