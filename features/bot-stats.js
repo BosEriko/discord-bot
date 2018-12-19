b161ca90261b62bf73f9c6cd99195f28e09ff85f
BosEriko <bos.eriko@gmail.com>
2018-12-19 14:57:02 +0800

    somebody keeps erasing my changes.

--- features/bot-stats.js
+++ features/bot-stats.js
@@ -1,21 +1,18 @@
 exports.botStats = (message, Client, firebase, symbolCommand) => {
-    // The Command
-    const command = message.cleanContent.split(" ")[0];
-    // The Parameter
-    const parameter = message.cleanContent.replace(command + " ", "");
-
     // Firebase Data
     const database = firebase.database();
     const statsData = database.ref().child('stats/' + message.author.id + '/data');
     let statsDataMessage;
-    statsData.once('value').then(snap => statsDataMessage = snap.exists() ? snap.val() : 0);
-    database.ref('stats/' + message.author.id).set({
-        message_count: statsDataMessage++
-    })
+    statsData.once('value').then(snap => {
+        statsDataMessage = snap.exists() ? snap.val() : 0;
+        database.ref('stats/' + message.author.id).set({
+            message_count: statsDataMessage++
+        });
+    });
 
     // The Main Stuff
     if (Client.user.id !== message.author.id) {
-        if(message.content.startsWith(symbolCommand + 'st-message')){
+        if (message.content.startsWith(symbolCommand + 'st-message')) {
             message.reply(statsDataMessage);
         }
     }
