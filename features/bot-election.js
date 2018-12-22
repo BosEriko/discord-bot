e2a68745a9573431357a5376ef0d4fd57a984ad8
BosEriko <bos.eriko@gmail.com>
2018-12-22 15:18:51 +0800

    Some bugs fixed

--- features/bot-election.js
+++ features/bot-election.js
@@ -1,26 +1,6 @@
 exports.botElection = (message, Client, firebase, symbolCommand) => {
-    // The Command
-    const command = message.cleanContent.split(" ")[0];
-    // The Parameter
-    const parameter = message.cleanContent.replace(command + " ", "");
-
-    // Firebase Data
-    const database = firebase.database();
-    // const electionData = database.ref().child('election/' + message.author.id + '/data');
-    // let electionDataValue;
-    // electionData.on("value", snap => {
-    //     electionDataValue = snap.exists() ? snap.val() : "No data yet";
-    // });
-
     // The Main Stuff
     if (message.channel.name === 'kuru-election' && Client.user.id !== message.author.id) {
-        if (command === (symbolCommand + 'write')) {
-            database.ref('election/' + message.author.id).set({
-                data: parameter
-            })
-        }else{
-            // message.reply(electionDataValue);
-            message.reply("Kuru Election is Under Maintenance.");
-        }
+        message.reply("Kuru Election is Under Maintenance.");
     }
 };
