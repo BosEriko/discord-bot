73ee33781a98988329072644218075f110bf1c3e
BosEriko <bos.eriko@gmail.com>
2018-12-22 16:54:52 +0800

    it is hump day _^_

--- features/bot-user-data.js
+++ features/bot-user-data.js
@@ -1,5 +1,5 @@
 exports.botUserData = (message, Client, firebase, symbolCommand, firebaseDatabase, Discord) => {
-    console.log("MESSAGE AUTHOR: ", message.member.nickname);
+    console.log("MESSAGE AUTHOR: ", (message.member.nickname === null ? "No nick" : message.member.nickname));
     // const userAccountRef = firebaseDatabase.child('user_account/' + (message.author.id));
     // userAccountRef.once('value').then(snap => {
     //     userAccountRef.set({
