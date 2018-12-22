4e40f6ebd80a3d60e1a0bb7c9387c2e9e63f1d26
BosEriko <bos.eriko@gmail.com>
2018-12-22 12:29:56 +0800

    Refactor factories, revisit visitors

--- features/bot-reputation.js
+++ features/bot-reputation.js
@@ -1,4 +1,5 @@
 exports.botReputation = (message, Client, firebase, symbolCommand) => {
+    const database = firebase.database();
     const userAccountRef = database.ref().child('user_account/' + (message.author.id + process.env.FIREBASE_ACCOUNT_EMAIL));
     userAccountRef.once('value').then(snap => {
         database.ref('user_account/' + (message.author.id + process.env.FIREBASE_ACCOUNT_EMAIL)).set({
