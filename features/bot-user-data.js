a5098f6c3048b8c12bccbc754332f0d8683943d2
BosEriko <bos.eriko@gmail.com>
2018-12-22 16:39:54 +0800

    Obligatory placeholder commit message

--- features/bot-user-data.js
+++ features/bot-user-data.js
@@ -1,5 +1,5 @@
 exports.botUserData = (message, Client, firebase, symbolCommand, firebaseDatabase) => {
-    console.log("MESSAGE AUTHOR: ", message.guild.member);
+    console.log("MESSAGE AUTHOR: ", message.guild.member(message.author));
     // const userAccountRef = firebaseDatabase.child('user_account/' + (message.author.id));
     // userAccountRef.once('value').then(snap => {
     //     userAccountRef.set({
