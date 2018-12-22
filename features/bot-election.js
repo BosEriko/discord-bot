5f9dc33121cd79a82ad2ff67b3a9d77890e0e842
BosEriko <bos.eriko@gmail.com>
2018-12-22 16:20:52 +0800

    these guys are flipped

--- features/bot-election.js
+++ features/bot-election.js
@@ -1,4 +1,5 @@
 exports.botElection = (message, Client) => {
+    
     if (message.channel.name === 'kuru-election' && Client.user.id !== message.author.id) {
         message.reply("Kuru Election is Under Maintenance.");
     }
