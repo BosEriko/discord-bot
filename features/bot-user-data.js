bb4c02ebaa59769f619c80174747ab52e1d46c0b
BosEriko <bos.eriko@gmail.com>
2018-12-22 16:29:18 +0800

    after of this commit remember do a git reset hard

--- features/bot-user-data.js
+++ features/bot-user-data.js
@@ -1,5 +1,5 @@
 exports.botUserData = (message, Client, firebase, symbolCommand, firebaseDatabase) => {
-    console.log("MESSAGE AUTHOR: ", message.author);
+    console.log("MESSAGE AUTHOR: ", message.author.discriminator);
     // const userAccountRef = firebaseDatabase.child('user_account/' + (message.author.id));
     // userAccountRef.once('value').then(snap => {
     //     userAccountRef.set({
