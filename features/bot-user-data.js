bf3073091d5d2640c9ce79478c9c65668c93b0b4
BosEriko <bos.eriko@gmail.com>
2018-12-22 16:42:09 +0800

    :q!

--- features/bot-user-data.js
+++ features/bot-user-data.js
@@ -1,5 +1,6 @@
-exports.botUserData = (message, Client, firebase, symbolCommand, firebaseDatabase) => {
-    console.log("MESSAGE AUTHOR: ", message.guild.member(message.author));
+exports.botUserData = (message, Client, firebase, symbolCommand, firebaseDatabase, Discord) => {
+    const user = new Discord.User(message.author);
+    console.log("MESSAGE AUTHOR: ", user.tag);
     // const userAccountRef = firebaseDatabase.child('user_account/' + (message.author.id));
     // userAccountRef.once('value').then(snap => {
     //     userAccountRef.set({
