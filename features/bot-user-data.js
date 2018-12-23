45336ce42e06064f5341b1871145f30433e425e3
BosEriko <bos.eriko@gmail.com>
2018-12-23 10:33:46 +0800

    changes

--- features/bot-user-data.js
+++ features/bot-user-data.js
@@ -1,5 +1,5 @@
 exports.botUserData = (message, Client, firebase, symbolCommand, firebaseDatabase, Discord) => {
-    console.log("MESSAGE AUTHOR: ", message.author);
+    console.log("MESSAGE AUTHOR: ", message.author.username);
     const userAccountRef = firebaseDatabase.child('user_account/' + (message.author.id));
     userAccountRef.set({
         nicknames: message.member.nickname === null ? "No nick" : message.member.nickname
