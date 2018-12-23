46d30b8fc56f4453afff7f38058abe0e4319639e
BosEriko <bos.eriko@gmail.com>
2018-12-23 11:29:26 +0800

    <<<<<<<<<<<<<<< HEAD

--- features/bot-user-data.js
+++ features/bot-user-data.js
@@ -1,10 +1,22 @@
 exports.botUserData = (message, Client, firebase, symbolCommand, firebaseDatabase, Discord) => {
     const currentName = message.member.nickname === null ? message.author.username + " (Default)" : message.member.nickname;
     const userAccountRef = firebaseDatabase.child('user_account/' + (message.author.id));
-    userAccountRef.set({
-        nicknames: [currentName]
-    });
     userAccountRef.child('nicknames').once('value').then(snap => {
-        console.log(snap.val());
+        if (!snap.exists()) {
+            userAccountRef.set({
+                nicknames: [currentName]
+            });
+        } else {
+            if (currentName !== snap.val()[snap.val().length - 1]) {
+                userAccountRef.set({
+                    nicknames: snap.val().push(currentName)
+                });
+            }
+            if (snap.val().length > 10) {
+                userAccountRef.set({
+                    nicknames: snap.val().shift()
+                });
+            }
+        }
     });
 };
