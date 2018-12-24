89dd812041950147075935ff668be09f3b0471aa
BosEriko <bos.eriko@gmail.com>
2018-12-24 17:52:05 +0800

    Trying to fake a conflict

--- features/bot-topic.js
+++ features/bot-topic.js
@@ -6,7 +6,7 @@ exports.botTopic = (message, Client, symbolCommand, firebaseDatabase) => {
     const day = Math.floor(diff / oneDay)
     if (message.channel.id === '510302403031990274' && Client.user.id !== message.author.id) {
         if (message.content.startsWith(symbolCommand + 'topic')) {
-            firebaseDatabase.child('fun_data/fornute_cookie').once('value').then(snap => {
+            firebaseDatabase.child('fun_data/topic_of_the_day').once('value').then(snap => {
                 let topicOfTheDay = snap.val()
                 message.reply(topicOfTheDay[day % topicOfTheDay.length])
             })
