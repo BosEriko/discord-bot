1370c5090ed39d6c39c3302ae314d7dcf4ff6eaa
BosEriko <bos.eriko@gmail.com>
2018-12-24 21:47:37 +0800

    Same as last commit with changes

--- features/bot-user-data.js
+++ features/bot-user-data.js
@@ -1,27 +1,25 @@
 exports.botUserData = (message, firebaseDatabase) => {
-    if (message.author.username !== 'Kuru Anime') {
-        const currentName = message.member.nickname === null ? message.author.username + " (Default)" : message.member.nickname
-        const userAccountRef = firebaseDatabase.child('user_account/' + (message.author.id))
-        userAccountRef.child('nicknames').once('value').then(snap => {
-            if (!snap.exists()) {
+    const currentName = message.author.username !== 'Kuru Anime' ? (message.member.nickname === null ? message.author.username : message.member.nickname) : 'Kuru Anime'
+    const userAccountRef = firebaseDatabase.child('user_account/' + (message.author.id))
+    userAccountRef.child('nicknames').once('value').then(snap => {
+        if (!snap.exists()) {
+            userAccountRef.set({
+                nicknames: [currentName]
+            })
+        } else {
+            let nicknamesArray = snap.val()
+            if (currentName !== nicknamesArray[nicknamesArray.length - 1]) {
+                nicknamesArray.push(currentName)
                 userAccountRef.set({
-                    nicknames: [currentName]
+                    nicknames: nicknamesArray
                 })
-            } else {
-                let nicknamesArray = snap.val()
-                if (currentName !== nicknamesArray[nicknamesArray.length - 1]) {
-                    nicknamesArray.push(currentName)
-                    userAccountRef.set({
-                        nicknames: nicknamesArray
-                    })
-                }
-                if (nicknamesArray.length > 10) {
-                    nicknamesArray.shift()
-                    userAccountRef.set({
-                        nicknames: nicknamesArray
-                    })
-                }
             }
-        })
-    }
+            if (nicknamesArray.length > 10) {
+                nicknamesArray.shift()
+                userAccountRef.set({
+                    nicknames: nicknamesArray
+                })
+            }
+        }
+    })
 }
