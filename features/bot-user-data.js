4ba31288bb8f24e850920490c8414693d55e407a
BosEriko <bos.eriko@gmail.com>
2018-12-24 13:00:31 +0800

    Shit code!

--- features/bot-user-data.js
+++ features/bot-user-data.js
@@ -1,27 +1,27 @@
 exports.botUserData = (message, firebaseDatabase) => {
     if (message.author.username !== 'Kuru Anime') {
-        const currentName = message.member.nickname === null ? message.author.username + " (Default)" : message.member.nickname;
-        const userAccountRef = firebaseDatabase.child('user_account/' + (message.author.id));
+        const currentName = message.member.nickname === null ? message.author.username + " (Default)" : message.member.nickname
+        const userAccountRef = firebaseDatabase.child('user_account/' + (message.author.id))
         userAccountRef.child('nicknames').once('value').then(snap => {
             if (!snap.exists()) {
                 userAccountRef.set({
                     nicknames: [currentName]
-                });
+                })
             } else {
-                let nicknamesArray = snap.val();
+                let nicknamesArray = snap.val()
                 if (currentName !== nicknamesArray[nicknamesArray.length - 1]) {
                     nicknamesArray.push(currentName)
                     userAccountRef.set({
                         nicknames: nicknamesArray
-                    });
+                    })
                 }
                 if (nicknamesArray.length > 10) {
-                    nicknamesArray.shift();
+                    nicknamesArray.shift()
                     userAccountRef.set({
                         nicknames: nicknamesArray
-                    });
+                    })
                 }
             }
-        });
+        })
     }
-};
+}
--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const Discord = require('discord.js');
-const ApiAI = require('apiai');
-const firebase = require('firebase');
-const Axios = require('axios');
+const Discord = require('discord.js')
+const ApiAI = require('apiai')
+const firebase = require('firebase')
+const Axios = require('axios')
 
-const Client = new Discord.Client();
-const App = ApiAI(process.env.DF_CLIENT_ACCESS_TOKEN);
+const Client = new Discord.Client()
+const App = ApiAI(process.env.DF_CLIENT_ACCESS_TOKEN)
 
 // Bot Modules
-const botFun = require('./features/bot-fun');
-const botElection = require('./features/bot-election');
-const botUserData = require('./features/bot-user-data');
-const botTopic = require('./features/bot-topic');
-const botPost = require('./features/bot-post');
-const botDF = require('./features/bot-df');
+const botFun = require('./features/bot-fun')
+const botElection = require('./features/bot-election')
+const botUserData = require('./features/bot-user-data')
+const botTopic = require('./features/bot-topic')
+const botPost = require('./features/bot-post')
+const botDF = require('./features/bot-df')
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -20,39 +20,39 @@ const firebaseConfig = {
     authDomain: process.env.FIREBASE_PROJECT_ID + '.firebaseapp.com',
     databaseURL: 'https://' + process.env.FIREBASE_DATABASE_NAME + '.firebaseio.com',
     storageBucket: process.env.FIREBASE_BUCKET + '.appspot.com',
-};
-firebase.initializeApp(firebaseConfig);
+}
+firebase.initializeApp(firebaseConfig)
 
 // Initialize Firebase Database
-const firebaseDatabase = firebase.database().ref(process.env.FIREBASE_PERSONAL_API_KEY);
+const firebaseDatabase = firebase.database().ref(process.env.FIREBASE_PERSONAL_API_KEY)
 
 // App Title
-const appTitle = 'Kuru Anime';
+const appTitle = 'Kuru Anime'
 
 // Symbol Command of Fun
-const symbolCommand = '~';
+const symbolCommand = '~'
 
 // Bot Ready Message
 Client.on('ready', () => {
-    console.log('Bot is ready.');
-});
+    console.log('Bot is ready.')
+})
 
 // New User
 Client.on('guildMemberAdd', member => {
     member.send('Hello! Thanks for joining ' + appTitle + '! Feel free to talk to me here or if you prefer, talk to me on the #kuru-anime channel. Don\'t forget to read our rules, okay? Have a nice stay!')
         .then(message => console.log(`Sent message: ${message.content}`))
-        .catch(console.error);
-});
+        .catch(console.error)
+})
 
 // Main Code
 Client.on('message', message => {
-    botFun.botFun(message, symbolCommand, Discord, Client, firebaseDatabase);
-    botElection.botElection(message, Client);
-    botUserData.botUserData(message, firebaseDatabase);
-    botTopic.botTopic(message, Client, symbolCommand);
-    botPost.botPost(message, Client, Axios);
-    botDF.botDF(message, Client, App);
-});
+    botFun.botFun(message, symbolCommand, Discord, Client, firebaseDatabase)
+    botElection.botElection(message, Client)
+    botUserData.botUserData(message, firebaseDatabase)
+    botTopic.botTopic(message, Client, symbolCommand)
+    botPost.botPost(message, Client, Axios)
+    botDF.botDF(message, Client, App)
+})
 
 // Discord Login
-Client.login(process.env.BOT_TOKEN);
+Client.login(process.env.BOT_TOKEN)
