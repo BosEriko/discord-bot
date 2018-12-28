a8984885de8a6695831c23bf3a43d99144bab9c2
BosEriko <bos.eriko@gmail.com>
2018-12-28 22:43:02 +0800

    grrrr

--- features/bot-topic.js
+++ features/bot-topic.js
@@ -1,10 +1,11 @@
 exports.botTopic = (firebaseDatabase, message, symbolCommand) => {
-    const now = new Date()
-    const start = new Date(now.getFullYear(), 0, 0)
-    const diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000)
-    const oneDay = 1000 * 60 * 60 * 24
-    const day = Math.floor(diff / oneDay)
     if (message.content.startsWith(symbolCommand + 'topic')) {
+        console.log("MESSAGE", message);
+        const now = new Date()
+        const start = new Date(now.getFullYear(), 0, 0)
+        const diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000)
+        const oneDay = 1000 * 60 * 60 * 24
+        const day = Math.floor(diff / oneDay)
         firebaseDatabase.child('fun_data/topic_of_the_day').once('value').then(snap => {
             let topicOfTheDay = snap.val()
             message.reply(topicOfTheDay[day % topicOfTheDay.length])
