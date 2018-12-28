64291a867bebaca62a6ed0842ac6b6bf18896b66
BosEriko <bos.eriko@gmail.com>
2018-12-28 10:55:06 +0800

    "Get that shit outta my master."

--- features/bot-video-only.js
+++ features/bot-video-only.js
@@ -1,6 +1,6 @@
 exports.botVideoOnly = (message) => {
     if (!message.cleanContent.match(/^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/)) {
+        message.author.send('Only YouTube videos are allowed on **#video**!')
         message.delete()
-        message.author.send('Only videos are allowed on **#video**!')
     }
 }
