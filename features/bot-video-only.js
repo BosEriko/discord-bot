25c6e16d70dfc169823166917e1c16d80901680b
BosEriko <bos.eriko@gmail.com>
2018-12-28 15:32:14 +0800

    Abandon all hope, ye who enter here.

--- features/bot-video-only.js
+++ features/bot-video-only.js
@@ -1,5 +1,5 @@
 exports.botVideoOnly = (message) => {
-    if (!message.cleanContent.match(/^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/)) {
+    if (!message.cleanContent.match(/^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/)) {
         message.author.send('Only YouTube videos are allowed on **#video**!')
         message.delete()
     }
