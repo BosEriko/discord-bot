30060aca3270cb6d0fd8c7d85bd8b3465171728d
BosEriko <bos.eriko@gmail.com>
2019-01-05 19:36:55 +0800

    Undoing last comming

--- scripts/list.js
+++ scripts/list.js
@@ -1,7 +1,7 @@
 exports.botList = (message) => {
     message.reply('this is a help message')
         .then(message => {
-            message.react(':arrow_left:')
+            message.react('👍')
         })
         .catch(() => {
             console.log('Error!');
