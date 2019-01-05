5ed80bf44e78be16a596dfa1059616427e22137e
BosEriko <bos.eriko@gmail.com>
2019-01-05 19:25:33 +0800

    stuff

--- scripts/list.js
+++ scripts/list.js
@@ -1,7 +1,7 @@
 exports.botList = (message) => {
     message.reply('this is a help message')
         .then(message => {
-            message.react('👍')
+            message.react('<:smiley:>')
         })
         .catch(() => {
             console.log('Error!');
