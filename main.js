f70839bb50b369fb405431eb40ce07514975ca2c
BosEriko <bos.eriko@gmail.com>
2019-02-04 20:31:39 +0800

    Bot under maintenance

--- main.js
+++ main.js
@@ -53,36 +53,37 @@ Client.on('guildMemberAdd', member => {
 
 // Main Code
 Client.on('message', message => {
-    // Passive Comands
-    if (message.channel.type !== 'dm' && Client.user.id !== message.author.id)
-        botPassive.botPassive(cooldownStorage, firebaseDatabase, message)
-    // Kuru Commands
-    if ((message.channel.id === '517566179242672137' || message.channel.id === '523150879562661892') && Client.user.id !== message.author.id)
-        botCommands.botCommands(Discord, firebaseDatabase, message, symbolCommand)
-    // Topic of the Day on #general
-    if (message.channel.id === '510302403031990274' && Client.user.id !== message.author.id)
-        botTopic.botTopic(firebaseDatabase, message, symbolCommand)
-    // Dialogflow Bot
-    if ((message.channel.id === '514736191095177246' || message.channel.type === 'dm') && Client.user.id !== message.author.id)
-        botDF.botDF(App, message)
-    // Post to #rabbit using the Webhook
-    if (message.channel.id === '526264102859964416' && Client.user.id !== message.author.id)
-        botPostRabbit.botPostRabbit(Axios, message)
-    // Post to #announcements using the Webhook
-    if (message.channel.id === '526264250230898698' && Client.user.id !== message.author.id)
-        botPostAnnouncements.botPostAnnouncements(Axios, message)
-    // Post to #waifu-rating using the Webhook
-    if (message.channel.id === '530348718952808449' && Client.user.id !== message.author.id)
-        botPostWaifuRating.botPostWaifuRating(Axios, message)
-    // All posts on #video will be deleted unless they are a YouTube url
-    if (message.channel.id === '528028883623346211' && Client.user.id !== message.author.id)
-        botVideoOnly.botVideoOnly(message)
-    // Kuru Market
-    if ((message.channel.id === '528252653390790676' || message.channel.id === '528253499893350430') && Client.user.id !== message.author.id)
-        botMarket.botMarket(appTitle, Discord, firebaseDatabase, message, symbolCommand)
-    // Kuru List
-    if ((message.channel.id === '531062120134082570') && Client.user.id !== message.author.id)
-        botList.botList(message)
+    message.reply('The bot is under maintenance!')
+    // // Passive Comands
+    // if (message.channel.type !== 'dm' && Client.user.id !== message.author.id)
+    //     botPassive.botPassive(cooldownStorage, firebaseDatabase, message)
+    // // Kuru Commands
+    // if ((message.channel.id === '517566179242672137' || message.channel.id === '523150879562661892') && Client.user.id !== message.author.id)
+    //     botCommands.botCommands(Discord, firebaseDatabase, message, symbolCommand)
+    // // Topic of the Day on #general
+    // if (message.channel.id === '510302403031990274' && Client.user.id !== message.author.id)
+    //     botTopic.botTopic(firebaseDatabase, message, symbolCommand)
+    // // Dialogflow Bot
+    // if ((message.channel.id === '514736191095177246' || message.channel.type === 'dm') && Client.user.id !== message.author.id)
+    //     botDF.botDF(App, message)
+    // // Post to #rabbit using the Webhook
+    // if (message.channel.id === '526264102859964416' && Client.user.id !== message.author.id)
+    //     botPostRabbit.botPostRabbit(Axios, message)
+    // // Post to #announcements using the Webhook
+    // if (message.channel.id === '526264250230898698' && Client.user.id !== message.author.id)
+    //     botPostAnnouncements.botPostAnnouncements(Axios, message)
+    // // Post to #waifu-rating using the Webhook
+    // if (message.channel.id === '530348718952808449' && Client.user.id !== message.author.id)
+    //     botPostWaifuRating.botPostWaifuRating(Axios, message)
+    // // All posts on #video will be deleted unless they are a YouTube url
+    // if (message.channel.id === '528028883623346211' && Client.user.id !== message.author.id)
+    //     botVideoOnly.botVideoOnly(message)
+    // // Kuru Market
+    // if ((message.channel.id === '528252653390790676' || message.channel.id === '528253499893350430') && Client.user.id !== message.author.id)
+    //     botMarket.botMarket(appTitle, Discord, firebaseDatabase, message, symbolCommand)
+    // // Kuru List
+    // if ((message.channel.id === '531062120134082570') && Client.user.id !== message.author.id)
+    //     botList.botList(message)
 })
 
 // Discord Login
