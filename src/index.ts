13ec91fcd1672e4de0710fe33766095973ec73a8
BosEriko <bos.eriko@gmail.com>
2019-02-14 20:04:28 +0800

    Herping the fucking derp right here and now.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
+// DiscordJS Import
 const Discord = require('discord.js')
 const Client = new Discord.Client()
 
+// Dialogflow Import
+const ApiAI = require('apiai')
+const App = ApiAI(process.env.DF_CLIENT_ACCESS_TOKEN)
+
+// Other Import
+const firebase = require('firebase')
+const Axios = require('axios')
+
+// Modules Import
+
+// Initialize Firebase
+const firebaseConfig: object = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_PROJECT_ID + '.firebaseapp.com',
+    databaseURL: 'https://' + process.env.FIREBASE_PROJECT_ID + '.firebaseio.com',
+    storageBucket: process.env.FIREBASE_PROJECT_ID + '.appspot.com',
+}
+firebase.initializeApp(firebaseConfig)
+
+// Bot Mount Event Trigger
 Client.on('ready', () => {
     console.log('Bot is ready.')
 })
 
+// Bot Message Event Trigger
 Client.on('message', (message: any) => {
     if (message.channel.type === 'dm' && Client.user.id !== message.author.id)
         message.reply("Under Maintenance!")
