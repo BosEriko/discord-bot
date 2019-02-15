92a1b5b80823aacf6aac59f2b63f7ae5a931ca3c
BosEriko <bos.eriko@gmail.com>
2019-02-15 09:23:05 +0800

    Final commit, ready for tagging

--- src/modules/dialogflow.ts
+++ src/modules/dialogflow.ts
@@ -1,5 +1,26 @@
 const modulesDialogflow: any = (data) => {
-    data.message.reply("DF Under Maintenance!")
+    let promise = new Promise((resolve, reject) => {
+        let request = data.df.textRequest(data.message.cleanContent, {
+            sessionId: data.message.author.id
+        })
+        request.on('response', (response) => {
+            console.log(response)
+            let rep = response.result.fulfillment.speech
+            resolve(rep)
+        })
+        request.on('error', (error) => {
+            resolve(null)
+        })
+        request.end()
+    })
+    ;(async function () {
+        let result = await promise
+        if (result) {
+            data.message.reply(result)
+        } else {
+            data.message.reply('nothing here')
+        }
+    }())
 }
 
 module.exports = modulesDialogflow
