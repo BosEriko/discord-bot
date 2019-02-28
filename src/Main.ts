ee5ef716089bb2a04cbdccc68eb7f1c2f0d6fa01
BosEriko <bos.eriko@gmail.com>
2019-02-28 18:20:48 +0800

    Fixed the comments being ugly. My fault

--- src/Main.ts
+++ src/Main.ts
@@ -1,33 +1,33 @@
-// =======================================================================================================[ Sentry Import ]===== //
+// Sentry Import
 import * as Sentry from '@sentry/browser';
 
-// ====================================================================================================[ DiscordJS Import ]===== //
+// DiscordJS Import
 const Discord = require('discord.js');
 const Client = new Discord.Client();
 
-// ===================================================================================================[ Dialogflow Import ]===== //
+// Dialogflow Import
 const df = require('apiai');
 const dfClient = df(process.env.DF_CLIENT_ACCESS_TOKEN);
 
-// ========================================================================================================[ Other Import ]===== //
+// Other Import
 const firebase = require('firebase');
 const axios = require('axios');
 
-// =====================================================================================================[ Webhooks Import ]===== //
+// Webhooks Import
 const webhooks: any = {
     announcements: require('./modules/webhooks/announcements'),
     rabbit: require('./modules/webhooks/rabbit'),
     waifuRating: require('./modules/webhooks/waifuRating'),
 };
 
-// ======================================================================================================[ Modules Import ]===== //
+// Modules Import
 const dialogflow: any = require('./modules/dialogflow');
 const prefix: any = require('./modules/prefix');
 
-// ===================================================================================================[ Initialize Sentry ]===== //
+// Initialize Sentry
 Sentry.init({ dsn: 'https://6777645d9bbd45a4bee12fa056c2d413@sentry.io/1394578' });
 
-// =================================================================================================[ Initialize Firebase ]===== //
+// Initialize Firebase
 const firebaseConfig: object = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_PROJECT_ID + '.firebaseapp.com',
@@ -36,20 +36,20 @@ const firebaseConfig: object = {
 };
 firebase.initializeApp(firebaseConfig);
 
-// ========================================================================================[ Initialize Firebase Database ]===== //
+// Initialize Firebase Database
 const database: any = firebase.database();
 
-// =================================================================================================[ Declare guildPrefix ]===== //
+// Declare guildPrefix
 let guildPrefix: string;
 
-// =============================================================================================[ Bot Mount Event Trigger ]===== //
+// Bot Mount Event Trigger
 Client.on('ready', () => {
     console.log('Bot is ready.');
 });
 
-// ===========================================================================================[ Bot Message Event Trigger ]===== //
+// Bot Message Event Trigger
 Client.on('message', (message: any) => {
-    // ======================================================================================================[ Parameters ]===== //
+    // Parameters
     let parameterSplit: any = message.cleanContent.split(" ");
     let parameters: object = {
         "$1": parameterSplit[1],
@@ -58,7 +58,7 @@ Client.on('message', (message: any) => {
         "$4": parameterSplit[4],
         "$5": parameterSplit[5],
     };
-    // ==================================================================================================[ Get the Prefix ]===== //
+    // Get the Prefix
     // database.ref('guild').child(`${message.guild.id}/prefix`).once('value').then((snap: any) => {
     //     if (snap.exists()) {
     //         guildPrefix = snap.val()
@@ -67,25 +67,25 @@ Client.on('message', (message: any) => {
     //         database.ref('guild').child(`${message.guild.id}/prefix`).set(guildPrefix)
     //     }
     // })
-    // =======================================================================[ Not Direct Message and not the bot itself ]===== //
+    // Not Direct Message and not the bot itself
     if (message.channel.type !== 'dm' && Client.user.id !== message.author.id) {
-        // =============================================================================================[ Kuru Anime Only ]===== //
+        // Kuru Anime Only
         if (message.guild.id === "510302403031990272") {
-            // =======================================================================================[ Rabbit Cross Post ]===== //
+            // Rabbit Cross Post
             if (message.channel.id === '526264102859964416') {
                 webhooks.rabbit({
                     axios: axios,
                     message: message,
                 });
             }
-            // =================================================================================[ Waifu Rating Cross Post ]===== //
+            // Waifu Rating Cross Post
             if (message.channel.id === '530348718952808449') {
                 webhooks.waifuRating({
                     axios: axios,
                     message: message,
                 });
             }
-            // ================================================================================[ Announcements Cross Post ]===== //
+            // Announcements Cross Post
             if (message.channel.id === '526264250230898698') {
                 webhooks.announcements({
                     axios: axios,
@@ -93,9 +93,9 @@ Client.on('message', (message: any) => {
                 });
             }
         }
-        // ===================================================================================================[ All Guild ]===== //
+        // All Guild
         if (true) {
-            // ===========================================================================================[ Modify Prefix ]===== //
+            // Modify Prefix
             // if (message.content.startsWith(`${guildPrefix}prefix`)) {
             //     prefix({
             //         message: message,
@@ -104,9 +104,9 @@ Client.on('message', (message: any) => {
             // }
         }
     }
-    // ===========================================================================[ Direct Message and not the bot itself ]===== //
+    // Direct Message and not the bot itself
     if (message.channel.type === 'dm' && Client.user.id !== message.author.id) {
-        // ==============================================================================================[ Dialogflow Bot ]===== //
+        // Dialogflow Bot
         dialogflow({
             df: dfClient,
             message: message,
@@ -114,5 +114,5 @@ Client.on('message', (message: any) => {
     }
 });
 
-// =======================================================================================================[ Discord Login ]===== //
+// Discord Login
 Client.login(process.env.BOT_TOKEN);
