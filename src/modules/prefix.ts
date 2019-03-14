2e8a0f3f279c304d77ce6231794d41f4b2aff614
BosEriko <bos.eriko@gmail.com>
2019-03-14 20:19:07 +0800

    Easteregg

--- src/modules/prefix.ts
+++ src/modules/prefix.ts
@@ -2,7 +2,7 @@
 const modulesPrefix: any = (data: any) => {
     if (data.command) {
         // data.database.ref('guild').child(`${data.message.guild.id}/prefix`).set(data.command);
-        data.message.reply(`Prefix has been set to ${data.args[1]}.`);
+        data.message.reply(`Prefix has been set to ${data.args[0]}.`);
     } else {
         data.message.reply(`Please specify a prefix.`);
     }
