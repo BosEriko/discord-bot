b11cb8e5b0d0ac403b41736861c540517c0afb80
BosEriko <bos.eriko@gmail.com>
2020-03-14 20:11:16 +0800

    fix warnings for closing curly brace and add user-info

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,15 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.content.startsWith(`${prefix}ping`)) {
     message.channel.send('Pong.');
-  } else if (message.content.startsWith(`${prefix}beep`)) {
+  }
+  else if (message.content.startsWith(`${prefix}beep`)) {
     message.channel.send('Boop.');
-  } else if (message.content === `${prefix}server`) {
+  }
+  else if (message.content === `${prefix}server`) {
     message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+  }
+  else if (message.content === `${prefix}user-info`) {
+    message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
   }
 });
 
