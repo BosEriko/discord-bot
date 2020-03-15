7318e8e6fc2720ae6bd920ac11efe69ec9dbe435
BosEriko <bos.eriko@gmail.com>
2020-03-15 18:05:20 +0800

    finish prune option

--- src/index.ts
+++ src/index.ts
@@ -67,6 +67,11 @@ client.on('message', message => {
     if (isNaN(amount)) {
       return message.reply('that doesn\'t seem to be a valid number.');
     }
+    else if (amount < 2 || amount > 100) {
+      return message.reply('you need to input a number between 2 and 100.');
+    }
+
+    message.channel.bulkDelete(amount);
   }
 });
 
