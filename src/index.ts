a601eb0ebe78f458a2d86b876ea5fb7ce4445dec
BosEriko <bos.eriko@gmail.com>
2020-03-15 18:08:11 +0800

    remove the prune message itself on the count

--- src/index.ts
+++ src/index.ts
@@ -62,13 +62,13 @@ client.on('message', message => {
     message.channel.send(avatarList);
   }
   else if (command === 'prune') {
-    const amount = parseInt(args[0]);
+    const amount = parseInt(args[0]) + 1;
 
     if (isNaN(amount)) {
       return message.reply('that doesn\'t seem to be a valid number.');
     }
-    else if (amount < 2 || amount > 100) {
-      return message.reply('you need to input a number between 2 and 100.');
+    else if (amount <= 1 || amount > 100) {
+      return message.reply('you need to input a number between 1 and 99.');
     }
 
     message.channel.bulkDelete(amount, true).catch(err => {
