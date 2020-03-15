9ebe054556586d9ae3a7c9d9ee94e9ce4fe4bad5
BosEriko <bos.eriko@gmail.com>
2020-03-15 11:48:28 +0800

    add kick mention

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,12 @@ client.on('message', message => {
     }
     message.channel.send(`First argument: ${args[0]}`);
   }
+  else if (command === 'kick') {
+    // grab the "first" mentioned user from the message
+    // this will return a `User` object, just like `message.author`
+    const taggedUser = message.mentions.users.first();
+    message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+  }
 });
 
 // Discord Login
