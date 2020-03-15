96770866a3a4e02a9a848f21bdf1e70e91ed9eb2
BosEriko <bos.eriko@gmail.com>
2020-03-15 17:58:34 +0800

    add a way to get avatar

--- src/index.ts
+++ src/index.ts
@@ -41,10 +41,18 @@ client.on('message', message => {
   }
   else if (command === 'kick') {
     if (!message.mentions.users.size) {
+      return message.reply('you need to tag a user in order to kick them!');
+    }
+    else {
       // grab the "first" mentioned user from the message
       // this will return a `User` object, just like `message.author`
       const taggedUser = message.mentions.users.first();
       message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    }
+  }
+  else if (command === 'avatar') {
+    if (!message.mentions.users.size) {
+      return message.channel.send(`Your avatar: ${message.author.avatar}`);
     }
   }
 });
