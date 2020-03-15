ee4d8a4d06505040b6d7b01d4dba93d3619c20e6
BosEriko <bos.eriko@gmail.com>
2020-03-15 18:09:13 +0800

    update current point

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Guide: https://discordjs.guide/
-// Current Point: https://discordjs.guide/creating-your-bot/commands-with-user-input.html#mentions
+// Current Point: https://discordjs.guide/command-handling/#individual-command-files
 // DiscordJS Import
 const discord = require('discord.js');
 const client = new discord.Client();
