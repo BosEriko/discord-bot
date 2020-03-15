220a53526a1c1261711024c92256b8d34b8ad13b
Bos Eriko Reyes <bos.eriko@gmail.com>
2020-03-16 00:16:02 +0800

    [Finishes #9] Create a welcome message that will link the user to the rules on the website #9
    # Story Title
    
    [Create a welcome message that will link the user to the rules on the website #9](https://github.com/kuru-project/discord-bot/issues/9)
    
    # Changes made
    
    - add admin rights for prune and add welcome message for joining users
    - update current point
    - remove the prune message itself on the count
    - add a catch for prune
    - finish prune option
    - add prune option
    - add multiple printing
    - add a way to get avatar
    - update current point
    - add a way to check if someone is tagged on kick or not
    - add kick mention
    - provide a way to avoid multiple spaces
    - update prefix
    - add args-info
    - add current point
    - minimize the code by using args and command
    - fix warnings for closing curly brace and add user-info
    - add total members count
    - use starts with instead
    - remove config.json
    - add prefix
    - use ts-node-dev in favor of nodemon
    - add nodemon
    - destructure config.json
    - create config json and require it to index.ts
    - add a prefix config.json
    - add ping/pong feature
    - update tabbing to spaces instead of tab
    - update linter rules
    - add eslintrc
    - add guide to index.ts
    - remove tslint flag on index.ts
    - install eslint
    
    # How does the solution address the problem
    
    This PR will add a welcome message for new users and a bunch of new commands

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,91 @@
-// tslint:disable: no-any
-
+// Guide: https://discordjs.guide/
+// Current Point: https://discordjs.guide/command-handling/#individual-command-files
 // DiscordJS Import
 const discord = require('discord.js');
 const client = new discord.Client();
+
+const prefix = ':';
+const adminID = '230249439481167872';
 
 // Bot Mount Event Trigger
 client.on('ready', () => {
   console.log('Bot is ready.');
 });
 
+// Bot Join Event Trigger
+client.on('guildMemberAdd', member => {
+  member.send('Welcome to the server!');
+});
+
 // Bot Message Event Trigger
-client.on('message', (message: any) => {
-  console.log("Message received.");
+client.on('message', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'ping') {
+    message.channel.send('Pong.');
+  }
+  else if (command === 'beep') {
+    message.channel.send('Boop.');
+  }
+  else if (command === 'server') {
+    message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+  }
+  else if (command === 'user-info') {
+    message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+  }
+  else if (command === 'args-info') {
+    if (!args.length) {
+      return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+    }
+    else if (args[0] === 'foo') {
+      return message.channel.send('bar');
+    }
+    message.channel.send(`First argument: ${args[0]}`);
+  }
+  else if (command === 'kick') {
+    if (!message.mentions.users.size) {
+      return message.reply('you need to tag a user in order to kick them!');
+    }
+    else {
+      // grab the "first" mentioned user from the message
+      // this will return a `User` object, just like `message.author`
+      const taggedUser = message.mentions.users.first();
+      message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    }
+  }
+  else if (command === 'avatar') {
+    if (!message.mentions.users.size) {
+      return message.channel.send(`Your avatar: ${message.author.avatar}`);
+    }
+    const avatarList = message.mentions.users.map(user => {
+      return `${user.username}'s avatar: ${message.author.avatar}`;
+    });
+    // send the entire array of strings as a message
+    // by default, discord.js will `.join()` the array with `\n`
+    message.channel.send(avatarList);
+  }
+  else if (command === 'prune') {
+    if (message.author.id !== adminID) {
+      return message.reply('You\' not an powerfule enough!');
+    }
+
+    const amount = parseInt(args[0]) + 1;
+
+    if (isNaN(amount)) {
+      return message.reply('that doesn\'t seem to be a valid number.');
+    }
+    else if (amount <= 1 || amount > 100) {
+      return message.reply('you need to input a number between 1 and 99.');
+    }
+
+    message.channel.bulkDelete(amount, true).catch(err => {
+      console.error(err);
+      message.channel.send('there was an error trying to prune messages in this channel!');
+    });
+  }
 });
 
 // Discord Login
