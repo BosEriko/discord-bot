1775af51ee15f53cda6170d8cde24f07ea79cd53
BosEriko <bos.eriko@gmail.com>
2020-03-15 18:01:52 +0800

    add multiple printing

--- src/index.ts
+++ src/index.ts
@@ -54,6 +54,12 @@ client.on('message', message => {
     if (!message.mentions.users.size) {
       return message.channel.send(`Your avatar: ${message.author.avatar}`);
     }
+    const avatarList = message.mentions.users.map(user => {
+      return `${user.username}'s avatar: ${message.author.avatar}`;
+    });
+    // send the entire array of strings as a message
+    // by default, discord.js will `.join()` the array with `\n`
+    message.channel.send(avatarList);
   }
 });
 
