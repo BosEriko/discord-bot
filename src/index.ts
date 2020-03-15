8364ed06f35ce47c2c54320c596a8200309e06a5
BosEriko <bos.eriko@gmail.com>
2020-03-15 11:14:06 +0800

    add args-info

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,15 @@ client.on('message', message => {
   else if (command === 'user-info') {
     message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
   }
+  else if (command === 'args-info') {
+    if (!args.length) {
+      return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+    }
+    else if (args[0] === 'foo') {
+      return message.channel.send('bar');
+    }
+    message.channel.send(`First argument: ${args[0]}`);
+  }
 });
 
 // Discord Login
