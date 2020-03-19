a4f9e56c969b7d9084cbdc488132311abf316dd0
Bos Eriko Reyes <bos.eriko@gmail.com>
2020-03-19 12:01:34 +0800

    [Finishes #12] Divide index.ts into commands
    # Story Title
    
    [Divide index.ts into commands](https://github.com/kuru-project/discord-bot/issues/12)
    
    # Changes made
    
    - move ping, server, and args-info to its own component
    - move avatar, kick and user-info to their own components
    - modularize the files
    - remove unwanted codes
    - remove the if-else chain
    - update checkpoint
    - remove .ts from file names
    - fix comments and add rules link on the welcome message
    
    # How does the solution address the problem
    
    This PR will divide the commands into separate files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
-// Guide: https://discordjs.guide/
-// Current Point: https://discordjs.guide/command-handling/#individual-command-files
-// DiscordJS Import
 const discord = require('discord.js');
 const client = new discord.Client();
 
-const prefix = ':';
+const prefix = ';';
 const adminID = '230249439481167872';
+
+client.commands = new discord.Collection();
+
+// List all arugments inside ./commands
+const commandFiles = [
+  'args-info',
+  'avatar',
+  'beep',
+  'kick',
+  'ping',
+  'prune',
+  'rules',
+  'server',
+  'user-info',
+];
+
+// Read commandFiles
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
 
 // Bot Mount Event Trigger
 client.on('ready', () => {
@@ -14,7 +32,7 @@ client.on('ready', () => {
 
 // Bot Join Event Trigger
 client.on('guildMemberAdd', member => {
-  member.send('Welcome to the server!');
+  member.send('Welcome to the server!\nRead our rules at https://web.kuru-anime.com/discord/rules');
 });
 
 // Bot Message Event Trigger
@@ -24,70 +42,14 @@ client.on('message', message => {
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if (command === 'ping') {
-    message.channel.send('Pong.');
-  }
-  else if (command === 'beep') {
-    message.channel.send('Boop.');
-  }
-  else if (command === 'server') {
-    message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-  }
-  else if (command === 'user-info') {
-    message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-  }
-  else if (command === 'args-info') {
-    if (!args.length) {
-      return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-    }
-    else if (args[0] === 'foo') {
-      return message.channel.send('bar');
-    }
-    message.channel.send(`First argument: ${args[0]}`);
-  }
-  else if (command === 'kick') {
-    if (!message.mentions.users.size) {
-      return message.reply('you need to tag a user in order to kick them!');
-    }
-    else {
-      // grab the "first" mentioned user from the message
-      // this will return a `User` object, just like `message.author`
-      const taggedUser = message.mentions.users.first();
-      message.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    }
-  }
-  else if (command === 'avatar') {
-    if (!message.mentions.users.size) {
-      return message.channel.send(`Your avatar: ${message.author.avatar}`);
-    }
-    const avatarList = message.mentions.users.map(user => {
-      return `${user.username}'s avatar: ${message.author.avatar}`;
-    });
-    // send the entire array of strings as a message
-    // by default, discord.js will `.join()` the array with `\n`
-    message.channel.send(avatarList);
-  }
-  else if (command === 'prune') {
-    if (message.author.id !== adminID) {
-      return message.reply('You\' not an powerfule enough!');
-    }
+  if (!client.commands.has(command)) return;
 
-    const amount = parseInt(args[0]) + 1;
-
-    if (isNaN(amount)) {
-      return message.reply('that doesn\'t seem to be a valid number.');
-    }
-    else if (amount <= 1 || amount > 100) {
-      return message.reply('you need to input a number between 1 and 99.');
-    }
-
-    message.channel.bulkDelete(amount, true).catch(err => {
-      console.error(err);
-      message.channel.send('there was an error trying to prune messages in this channel!');
-    });
+  try {
+    client.commands.get(command).execute(message, args, adminID);
   }
-  else if (command === 'rules') {
-    message.channel.send('Read our rules at https://web.kuru-anime.com/discord/rules');
+  catch (error) {
+    console.error(error);
+    message.reply('There was an error trying to execute that command!');
   }
 });
 
