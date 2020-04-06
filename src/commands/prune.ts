4016d3794f812842ddf90b2ad9b370e6a57f6eb0
BosEriko <bos.eriko@gmail.com>
2020-04-06 19:59:37 +0800

    Fix the typo and update the other messages to Uppercase first letter

--- src/commands/prune.ts
+++ src/commands/prune.ts
@@ -3,21 +3,21 @@ module.exports = {
   description: 'Prune Messages',
   execute(message, args, adminID) {
     if (message.author.id !== adminID) {
-      return message.reply('You\' not an powerfule enough!');
+      return message.reply('You\'re not an powerful enough!');
     }
 
     const amount = parseInt(args[0]) + 1;
 
     if (isNaN(amount)) {
-      return message.reply('that doesn\'t seem to be a valid number.');
+      return message.reply('That doesn\'t seem to be a valid number.');
     }
     else if (amount <= 1 || amount > 100) {
-      return message.reply('you need to input a number between 1 and 99.');
+      return message.reply('You need to input a number between 1 and 99.');
     }
 
     message.channel.bulkDelete(amount, true).catch(err => {
       console.error(err);
-      message.channel.send('there was an error trying to prune messages in this channel!');
+      message.channel.send('There was an error trying to prune messages in this channel!');
     });
   },
 };
